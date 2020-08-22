15f883d4e4dcd759a7a49c516bbdb6c8ca1e66bc
Administrator <admin@example.com>
2020-08-22 21:46:21 +0530

    added more code like /version

--- backup.py
+++ backup.py
@@ -6,18 +6,8 @@ import tornado.web
 import random
 import time
 import socket
-import platform
 from tornado.escape import json_encode
 
-
-def getSName():
-  try:
-    hostSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    hostSocket.connect(("8.8.8.8", 80))
-    return (hostSocket.getsockname()[0])
-    hostSocket.close()
-  except Exception, e:
-     print e  
 
 class Info(tornado.web.RequestHandler):
   def get(self):
@@ -25,22 +15,23 @@ class Info(tornado.web.RequestHandler):
     Handles `/info` resource.
     """
     try:
-     hostname = platform.node()
-     IPAddr = getSName()
-     logging.info("/info serving from %s has been invoked from %s \n", hostname, IPAddr)
-     self.set_header("Content-Type", "application/json")
-     self.write(json_encode(
+      hostname = socket.gethostname()    
+      IPAddr = socket.gethostbyname(hostname)    
+      logging.info("/info serving from %s has been invoked from %s \n", self.request.host, self.request.remote_ip)
+      self.set_header("Content-Type", "application/json")
+      self.write(json_encode(
         {
-          "ip Address": IPAddr,
-          "hostname": hostname, 
-          "host" : self.request.host,
-          "from" : self.request.remote_ip
+          "version" : VERSION,
+          "host" : hostname,
+          "type" : "apple",
+          "from" : IPAddr
         }
       ))
-     self.finish()
+      self.finish()
     except Exception, e:
-     logging.debug(e)
-     self.set_status(404)
+      logging.debug(e)
+      self.set_status(404)
+
 
 class Version(tornado.web.RequestHandler):
   def get(self):
@@ -48,16 +39,15 @@ class Version(tornado.web.RequestHandler):
     Handles `/version` resource.
     """
     try:
-      hostname = platform.node()
-      IPAddr = getSName()
+      hostname = socket.gethostname()
+      IPAddr = socket.gethostbyname(hostname)
       logging.info("/version serving from %s has been invoked from %s \n", self.request.host, self.request.remote_ip)
       self.set_header("Content-Type", "application/json")
       self.write(json_encode(
-        { 
-          "Fruit Version" : "banana",
-          "VERSION": VERSION,
-          "ip Address": IPAddr,
-          "hostname": hostname
+        {
+          "version" : "banana",
+          "host" : hostname,
+          "from" : IPAddr
         }
       ))
       self.finish()
@@ -65,6 +55,8 @@ class Version(tornado.web.RequestHandler):
       logging.debug(e)
       print e
       self.set_status(404)
+
+
 
 class Environment(tornado.web.RequestHandler):
   def get(self):
@@ -86,11 +78,10 @@ class Environment(tornado.web.RequestHandler):
       self.set_status(404)
 
 
-
 if __name__ == "__main__":
   app = tornado.web.Application([
         (r"/info", Info),
-        (r"/ver", Version),
+        (r"/version", Version),
         (r"/env", Environment)
   ])
  
@@ -101,4 +92,3 @@ if __name__ == "__main__":
   app.listen(PORT, address='0.0.0.0')
   logging.info("This is simple service in version v%s listening on port %s", VERSION, PORT)
   tornado.ioloop.IOLoop.current().start()
-
